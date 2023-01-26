5d6ef4f5448fa4710d64c423a704c75b7fd138da
tlshudson <hudsonteles00@gmail.com>
2023-01-26 18:02:14 -0300

    2a atualização main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-document.querySelector('.tecla_pom')
+function reproduzSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+    
+}
 
-//nesta aula aprendi sobre as mais básicas funcionalidades do JavaScript
-//aprendi sobre o onclick seguido de alert retornando um alerta na tela do usuário
-//aprendi a selecionar um documento>elemento utilizando a função~
-//document.querySelector
+document.querySelector('.tecla_pom').onclick=reproduzSomPom;
