e3bfec5af219295cc1b2d8df4ba1d28b3e96b712
tlshudson <hudsonteles00@gmail.com>
2023-01-26 17:07:15 -0300

    1a atualização

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.tecla_pom')
+
+//nesta aula aprendi sobre as mais básicas funcionalidades do JavaScript
+//aprendi sobre o onclick seguido de alert retornando um alerta na tela do usuário
+//aprendi a selecionar um documento>elemento utilizando a função~
+//document.querySelector
