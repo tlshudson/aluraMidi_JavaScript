3a59d7033f71608f3280919d36b53d2be4b9ad02
tlshudson <tlshudson@DESKTOP-KEP6PRS>
2023-02-04 15:09:09 -0300

    4a atualizacao

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function reproduzSomPom (){
     document.querySelector('#som_tecla_pom').play();
-    
 }
+//função que seleciona o arquivo de som que reproduz a tecla pom
+//logo em seguida reproduz o arquivo de som
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = reproduzSomPom;
+//constante que armazena 
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = reproduzSomPom;
+    
+    contador = contador + 1;
+    console.log(contador);
+}
