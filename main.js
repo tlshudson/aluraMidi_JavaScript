e3b340edfe5791963868e49f0b6608e4c4f80675
tlshudson <tlshudson@DESKTOP-KEP6PRS>
2023-02-10 20:35:47 -0300

    5ª atualizaçaão

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-function reproduzSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function reproduzSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-//função que seleciona o arquivo de som que reproduz a tecla pom
-//logo em seguida reproduz o arquivo de som
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-//constante que armazena 
+//constante que armazena o id de todas as teclas do arquivo html
 
 let contador = 0;
+//variável que armazena o número de teclas
 
+//enquanto o valor armazenado na variavel contador for menor que ao tamanho lista de teclas
 while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = reproduzSomPom;
-    
+
+    const teclas = listaDeTeclas[contador];
+    const instrumento = teclas.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    teclas.onclick = function () {
+        reproduzSom(idAudio);
+    }
+
+    //limitador do contador para ele não ultrapassar a quantidade de teclas
     contador = contador + 1;
-    console.log(contador);
 }
